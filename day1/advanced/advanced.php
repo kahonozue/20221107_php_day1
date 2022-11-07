97c87a42851cc70ab803c15f28d441bd9885c953
kahonozue <smilekaho@gmail.com>
2022-11-07 21:07:12 +0100

    update my advanced exercise

--- day1/advanced/advanced.php
+++ day1/advanced/advanced.php
@@ -1,15 +1,5 @@
 <!-- http://localhost/php-exercises/day1/advanced/advanced.php -->
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/advanced.css">
-    <title>advanced</title>
-</head>
-<body>
 <!-- Define an associative array and output array elements on the screen. Use for this cartoon/anime/game characters 
 (like Mickey Mouse, Super Mario, Goku, Pokemon etc.). 
 
@@ -19,23 +9,24 @@ Transform this code into a solution done with multidimensional arrays to track d
 
 // associative array
 $associative_array = array(
-    "cartoon" => "Mickey_Mouse",
+    "cartoon" => "MickeyMouse",
     "anime" => "Doraemon",
     "game" => "Mario"
 );  
 
-echo "<p class='subtitle'>associative array</p>";
+// echo "<p class='subtitle'>associative array</p>";
 // "echo" doesn't necessary below cuz it means I'm doing "print_r" twice.
 // echo print_r($associative_array) brings two results; result of "print_r($associative_array)" and result of "$associative_array".
-print_r($associative_array);
-echo "<br>";
-print_r($associative_array["cartoon"]);
-echo ",";
-print_r($associative_array["anime"]);
-echo ",";
-print_r($associative_array["game"]);
-echo "<br>";
-echo "<br>";
+// print_r($associative_array);
+// echo "<br>";
+
+// print_r($associative_array["cartoon"]);
+// echo ",";
+// print_r($associative_array["anime"]);
+// echo ",";
+// print_r($associative_array["game"]);
+// echo "<br>";
+// echo "<br>";
 
 
 // multidimensional array
@@ -57,28 +48,60 @@ $multidimensional_array = array(
     ),
 );
 
-echo "<p class='subtitle'>multidimensional array</p>";
-print_r($multidimensional_array['Mickey_Mouse']["personality_one"]);
-echo ", ";
-print_r($multidimensional_array['Mickey_Mouse']["personality_two"]);
-echo ", ";
-print_r($multidimensional_array['Mickey_Mouse']["personality_three"]);
-echo "<br>";
+// echo "<p class='subtitle'>multidimensional array</p>";
+// print_r($multidimensional_array['Mickey_Mouse']["personality_one"]);
+// echo ", ";
+// print_r($multidimensional_array['Mickey_Mouse']["personality_two"]);
+// echo ", ";
+// print_r($multidimensional_array['Mickey_Mouse']["personality_three"]);
+// echo "<br>";
 
-print_r($multidimensional_array['Doraemon']["personality_one"]);
-echo ", ";
-print_r($multidimensional_array['Doraemon']["personality_two"]);
-echo ", ";
-print_r($multidimensional_array['Doraemon']["personality_three"]);
-echo "<br>";
+// print_r($multidimensional_array['Doraemon']["personality_one"]);
+// echo ", ";
+// print_r($multidimensional_array['Doraemon']["personality_two"]);
+// echo ", ";
+// print_r($multidimensional_array['Doraemon']["personality_three"]);
+// echo "<br>";
 
-print_r($multidimensional_array['Mario']["personality_one"]);
-echo ", ";
-print_r($multidimensional_array['Mario']["personality_two"]);
-echo ", ";
-print_r($multidimensional_array['Mario']["personality_three"]);;
+// print_r($multidimensional_array['Mario']["personality_one"]);
+// echo ", ";
+// print_r($multidimensional_array['Mario']["personality_two"]);
+// echo ", ";
+// print_r($multidimensional_array['Mario']["personality_three"]);;
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/advanced.css">
+    <title>advanced</title>
+</head>
+<body>
+    <div class="contentBlock">
+        <div class="characterBlock">
+            <div>
+                <?= $associative_array["cartoon"] ?>
+                <img src="mickey_mouse.jpg" arc="micky mouse">
+            </div>
+        </div>
+        <div class="characterBlock">
+            <div>
+                <?= $associative_array["anime"] ?>
+                <img src="Doraemon.png" arc="doraemon">
+            </div>
+        </div>
+        <div class="characterBlock">
+            <div>
+                    <?= $associative_array["game"] ?>
+            <img src="mario.jpg" arc="mario">
+            </div>
+        </div>
+    </div>
 
     
 </body>
